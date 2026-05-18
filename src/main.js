44503286d1b51357927b121786144fe7294e61ee
Andres Barrios <anmabadi@outlook.com>
2026-05-17 23:21:42 -0500

    agregado switch para autenticacion

--- src/main.js
+++ src/main.js
@@ -32,70 +32,70 @@ const dataUser = [
   {
     id: 1,
     name: "Alejandro",
-    role: "coder",
+    role: "Coder",
     email: "alejandro.villanueva@riwi.io",
     password: "coder123",
   },
   {
     id: 2,
     name: "Abrahan",
-    role: "team leader",
+    role: "Team Leader",
     email: "abrahan.villa@riwi.io",
     password: "teamleader123",
   },
   {
     id: 3,
     name: "Camila",
-    role: "designer",
+    role: "English Coach",
     email: "camila.rojas@riwi.io",
     password: "designer123",
   },
   {
     id: 4,
     name: "Sebastian",
-    role: "backend developer",
+    role: "Coder",
     email: "sebastian.mora@riwi.io",
     password: "backend123",
   },
   {
     id: 5,
     name: "Valentina",
-    role: "frontend developer",
+    role: "English Coach",
     email: "valentina.gomez@riwi.io",
     password: "frontend123",
   },
   {
     id: 6,
     name: "Juan",
-    role: "qa tester",
+    role: "Coder",
     email: "juan.perez@riwi.io",
     password: "qatester123",
   },
   {
     id: 7,
     name: "Laura",
-    role: "project manager",
+    role: "Team Leader",
     email: "laura.castillo@riwi.io",
     password: "manager123",
   },
   {
     id: 8,
     name: "Daniel",
-    role: "devops",
+    role: "Coder",
     email: "daniel.ramirez@riwi.io",
     password: "devops123",
   },
   {
     id: 9,
     name: "Sofia",
-    role: "ux/ui designer",
+    role: "English Coach",
     email: "sofia.martinez@riwi.io",
     password: "uxui123",
   },
   {
     id: 10,
     name: "Mateo",
-    role: "data analyst",
+    role: "Team Leader",
     email: "mateo@riwi.io",
     password: "data123",
   },
@@ -107,11 +107,11 @@ if (saveUser){
   console.log(saveUser);
   
 }else{
-  loginView()
+  loginBox();
 }
 
-function loginInit(){
-  const loginForm = getElementById('login'); // Se captura el form despues de que loginView() lo cree
+function loginValidation(){
+  const loginForm = getElementById('login'); // Se captura el form despues de que loginBox() lo cree
 
   loginForm.addEventListener('submit', e =>{ // e (event) is the object of the event, it takes the report of what just happend so it can execute the function inside.
     e.preventDefault();
@@ -128,26 +128,52 @@ function loginInit(){
 
       console.log(store);
     }else{
-      const alertMsg = getElementById('error')
+      const alertMsg = getElementById('error');
       alertMsg.innerHTML = `
       <span id="alert-msg" class="text-red-700 ">Either the username and/or password is incorrect</span>
-      `
+      `;
     }
   });
 }
 
 function loginWelcome(user){
-  app.innerHTML = `<p>Welcome ${user.name}</p>
-  <button id="btn-logout" class="bg-blue-950 cursor-pointer text-amber-50 mt-2 p-2 rounded-md self-center ml-2">Logout</button>
-  `;
+  // app.innerHTML = `<p>Welcome ${user.name}</p>
+  // <button id="btn-logout" class="bg-blue-950 cursor-pointer text-amber-50 mt-2 p-2 rounded-md self-center ml-2">Logout</button>
+  // `;
   
-  document.querySelector('#btn-logout').addEventListener('click', ()=>{
+  // document.querySelector('#btn-logout').addEventListener('click', ()=>{
+  //   store.logout();
+  //   loginBox();
+  // });
+  switch (user.role) {
+    case "Team Leader":
+      app.innerHTML = `
+      <h1>Team Leader dashboard</h1>
+      <p>Welcome ${user.name}</p>
+      <button id="btn-logout" class="bg-blue-950 cursor-pointer text-amber-50 mt-2 p-2 rounded-md self-center ml-2">Logout</button>
+      `;
+      break;
+    case "English Coach":
+      app.innerHTML = `
+      <h1>English Coach dashboard</h1>
+      <p>Welcome ${user.name}</p>
+      <button id="btn-logout" class="bg-blue-950 cursor-pointer text-amber-50 mt-2 p-2 rounded-md self-center ml-2">Logout</button>
+      `;
+      break;
+    default:
+      app.innerHTML = `
+      <p>Welcome ${user.name}</p>
+      <button id="btn-logout" class="bg-blue-950 cursor-pointer text-amber-50 mt-2 p-2 rounded-md self-center ml-2">Logout</button>
+      `;
+      break;
+  }
+  getElementById('btn-logout').addEventListener('click', ()=>{
     store.logout();
-    loginView();
-  });
+    loginBox();
+  })
 }
 
-function loginView(){ // el navegador borra todo lo que había en el .html y crea elementos nuevos.
+function loginBox(){ // el navegador borra todo lo que había en el .html y crea elementos nuevos.
    app.innerHTML = ` 
   <form id="login" class="flex flex-col bg-gray-300 p-5 rounded-lg w-70">
 
@@ -172,168 +198,5 @@ function loginView(){ // el navegador borra todo lo que había en el .html y cre
   
   </form>
 `
-   loginInit()
+   loginValidation();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import './style.css';
-// import { getElementById } from './utils/getElementById';
-
-// const login = getElementById('login'); // Cleaner code 
-// const app = getElementById('app');
-
-// const store = {
-//   user : null, // reserve space in memory to use later
-//   isLogin : false,
-
-//   login(data){ // is made the parameter later will pass as an argument
-//     this.user = data;
-//     this.isLogin = true;
-//   },
-
-//   // logout(){
-//   //   this.user = null;
-//   //   this.isLogin = false;
-//   //   localStorage.removeItem('user');
-//   // },
-  
-//   saveData(data){ // same here (data is what user is entering in login)
-//     /*propierty of 
-//     "window" 
-//     object 
-//     (browser)*///- the save command 
-//     localStorage.setItem('user', JSON.stringify(data))
-//   }
-// }
-
-// const dataUser = [
-//   {
-//     id: 1,
-//     name: "Alejandro",
-//     role: "coder",
-//     email: "alejandro.villanueva@riwi.io",
-//     password: "coder123",
-//   },
-//   {
-//     id: 2,
-//     name: "Abrahan",
-//     role: "team leader",
-//     email: "abrahan.villa@riwi.io",
-//     password: "teamleader123",
-//   },
-//   {
-//     id: 3,
-//     name: "Camila",
-//     role: "designer",
-//     email: "camila.rojas@riwi.io",
-//     password: "designer123",
-//   },
-//   {
-//     id: 4,
-//     name: "Sebastian",
-//     role: "backend developer",
-//     email: "sebastian.mora@riwi.io",
-//     password: "backend123",
-//   },
-//   {
-//     id: 5,
-//     name: "Valentina",
-//     role: "frontend developer",
-//     email: "valentina.gomez@riwi.io",
-//     password: "frontend123",
-//   },
-//   {
-//     id: 6,
-//     name: "Juan",
-//     role: "qa tester",
-//     email: "juan.perez@riwi.io",
-//     password: "qatester123",
-//   },
-//   {
-//     id: 7,
-//     name: "Laura",
-//     role: "project manager",
-//     email: "laura.castillo@riwi.io",
-//     password: "manager123",
-//   },
-//   {
-//     id: 8,
-//     name: "Daniel",
-//     role: "devops",
-//     email: "daniel.ramirez@riwi.io",
-//     password: "devops123",
-//   },
-//   {
-//     id: 9,
-//     name: "Sofia",
-//     role: "ux/ui designer",
-//     email: "sofia.martinez@riwi.io",
-//     password: "uxui123",
-//   },
-//   {
-//     id: 10,
-//     name: "Mateo",
-//     role: "data analyst",
-//     email: "mateo@riwi.io",
-//     password: "data123",
-//   },
-// ];
-
-// login.addEventListener('submit', e =>{ // e (event) is the object of the event, it takes the report of what just happend so it can execute the function inside.
-//     e.preventDefault();
-
-//     const {email, password} = Object.fromEntries(new FormData(login));
-
-//     const getUser = dataUser.find(user => user.email === email || user.password === password);
-
-//     console.log(getUser);
-    
-//     if (email === getUser?.email && password === getUser?.password){ // The optional concatenation operator “?” prevents the code from crashing
-//       app.innerHTML = `<p>Welcome ${getUser.name}</p>
-//       <button id="btn-logout" class="bg-blue-950 cursor-pointer text-amber-50 mt-2 p-2 rounded-md self-center ml-2">Logout</button>
-//       `
-//       document.querySelector('#btn-logout').addEventListener('click', ()=>{
-//         // store.logout()
-//       });
-//       store.saveData(getUser);
-//       store.login(getUser);
-//       console.log(store);
-//     }else{
-//       alert('Either the password and/or the username is incorrect')
-//     }
-// })
